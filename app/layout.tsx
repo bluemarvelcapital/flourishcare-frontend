e4bebd23312e994bc127761b52b5fafde9e830d3
RealRichi3 <molunorichie@gmail.com>
2023-12-23 22:16:56 +0100

    submit user form

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,26 +4,29 @@ import "./keyframes.css"
 import "animate.css/animate.min.css"
 import { Header } from "@/components/Header"
 import { Footer } from "@/components/Footer"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const inter = Inter({ subsets: ["latin"] })
 const open_Sans = Open_Sans({
-  style: "normal",
-  subsets: ["latin"],
-  weight: ["300", "500", "400", "700"],
+    style: "normal",
+    subsets: ["latin"],
+    weight: ["300", "500", "400", "700"],
 })
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`font-myriad-pro bg-baseLight`}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={`font-myriad-pro bg-baseLight`}>
+                <Header />
+                <ToastContainer position={'top-center'} />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    )
 }
