9980e4e0d69f592d0db8ec5bdae1f02a83ca15de
RealRichi3 <molunorichie@gmail.com>
2024-01-03 18:48:19 +0100

    fix: typo 'flourished -> Flourish'

--- app/about-us/Hero.tsx
+++ app/about-us/Hero.tsx
@@ -7,45 +7,45 @@ import Fade from "react-reveal/Fade"
 import { Avatar } from "antd"
 
 export const Hero = () => {
-  return (
-    <div className="relative w-full md:h-[95vh] h-[50vh] hero-h">
-      <div className="absolute w-full h-full top-0 left-0 z-[-2] ">
-        <Image
-          src={"/home-hero2.svg"}
-          alt="Flourish Care Hero"
-          fill={true}
-          objectFit="cover"
-        />
-      </div>
-      <div className="absolute top-0 left-0 w-full h-full bg-[#0000009d] z-[-1]"></div>
-
-      <Fade left>
-        <div className="text-center md:w-[70%] w-[90%] mx-[auto] md:py-[154px] md:px-[64px] py-[1.5rem] px-[16px]">
-          <h1 className="md:text-[65px] text-[24px] text-white font-[800]">
-            About Us
-          </h1>
-          <h1 className="md:text-[55px] text-[18px] text-white font-[800]">
-            Your Trusted Care Partner
-          </h1>
-          <p className="md:text-[26px] md:leading-[141%] text-white mt-[1rem] md:w-[80%] mx-[auto]">
-            Flourished Advanced Care is rooted in the mission to deliver care
-            with integrity and compassion. Our team is carefully selected and
-            trained to match the needs of our clients with the utmost respect
-            and professionalism.
-          </p>
-
-          <Link href="/contact-us">
-            <Button className="md:my-[52px] my-3">Contact Us</Button>
-          </Link>
-          <div className="flex items-center gap-[20px] justify-center text-white">
-            <Avatar src="/avatar.svg" className="w-[63px] h-[63px]" />
-            <div>
-              <p className="text-start md:text-[19px]">Call Helen:</p>
-              <p className="text-start md:text-[19px]">(+44)00000000</p>
+    return (
+        <div className="relative w-full md:h-[95vh] h-[50vh] hero-h">
+            <div className="absolute w-full h-full top-0 left-0 z-[-2] ">
+                <Image
+                    src={"/home-hero2.svg"}
+                    alt="Flourish Care Hero"
+                    fill={true}
+                    objectFit="cover"
+                />
             </div>
-          </div>
+            <div className="absolute top-0 left-0 w-full h-full bg-[#0000009d] z-[-1]"></div>
+
+            <Fade left>
+                <div className="text-center md:w-[70%] w-[90%] mx-[auto] md:py-[154px] md:px-[64px] py-[1.5rem] px-[16px]">
+                    <h1 className="md:text-[65px] text-[24px] text-white font-[800]">
+                        About Us
+                    </h1>
+                    <h1 className="md:text-[55px] text-[18px] text-white font-[800]">
+                        Your Trusted Care Partner
+                    </h1>
+                    <p className="md:text-[26px] md:leading-[141%] text-white mt-[1rem] md:w-[80%] mx-[auto]">
+                        Flourish Advanced Care is rooted in the mission to deliver care
+                        with integrity and compassion. Our team is carefully selected and
+                        trained to match the needs of our clients with the utmost respect
+                        and professionalism.
+                    </p>
+
+                    <Link href="/contact-us">
+                        <Button className="md:my-[52px] my-3">Contact Us</Button>
+                    </Link>
+                    <div className="flex items-center gap-[20px] justify-center text-white">
+                        <Avatar src="/avatar.svg" className="w-[63px] h-[63px]" />
+                        <div>
+                            <p className="text-start md:text-[19px]">Call Helen:</p>
+                            <p className="text-start md:text-[19px]">(+44)00000000</p>
+                        </div>
+                    </div>
+                </div>
+            </Fade>
         </div>
-      </Fade>
-    </div>
-  )
+    )
 }
