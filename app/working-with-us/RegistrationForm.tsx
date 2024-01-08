f6dd40e4c7909adaa18ea46a14f2a111793183c9
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-08 23:48:58 +0100

    add can work in the UK

--- app/working-with-us/RegistrationForm.tsx
+++ app/working-with-us/RegistrationForm.tsx
@@ -20,7 +20,8 @@ interface RegistrationFormProps {
 
 export const RegistrationForm: React.FC<RegistrationFormProps> = ({ next }) => {
   const [hasOtherName, setHasOtherName] = React.useState<"yes" | "no" | "">("")
-  const [isOverAged18, setIsOverAged18] = React.useState<"yes" | "no" | "">("")
+  const [isOverAge18, setIsOverAge18] = React.useState<"yes" | "no" | "">("")
+  const [canWorkInUK, setCanWorkInUK] = React.useState<"yes" | "no" | "">("")
 
   return (
     <div className="py-10 px-6 bg-white transition-all">
