7bbe959a25dfc1b2e8379f0c4c02389288192379
Osaretin Frank <osaretin.frank10@gmail.com>
2024-01-10 22:32:17 +0100

    add step 1 form for international candidates

--- app/working-with-us/InternationFormSteps.tsx
+++ app/working-with-us/InternationFormSteps.tsx
@@ -1,5 +1,36 @@
-import React from "react"
+"use client"
+import {
+  AppRegistrationOutlined,
+  BadgeOutlined,
+  EditNoteOutlined,
+  MedicalInformation,
+} from "@mui/icons-material"
+import React, { useState } from "react"
+import { HealthForm } from "./HealthForm"
+import { EmployeeForm } from "./EmployeeForm"
+import { NonDisclosureForm } from "./NonDisclosureForm"
+import { InternationalStep1 } from "./InternationalStep1"
 
 export const InternationFormSteps = () => {
-  return <div>InternationFormSteps</div>
+  const [activeStep, setActiveStep] = useState(0)
+  let next = () => setActiveStep((prev) => prev + 1)
+  const items = [
+    {
+      content: <InternationalStep1 next={next} />,
+    },
+    {
+      content: <HealthForm next={next} />,
+    },
+    {
+      content: <EmployeeForm next={next} />,
+    },
+    {
+      content: <NonDisclosureForm />,
+    },
+  ]
+  return (
+    <div className="mt-10 form-steps">
+      <div className="md:p-10 my-[2rem]">{items[activeStep].content}</div>
+    </div>
+  )
 }
---
+++ app/working-with-us/InternationalStep1.tsx
@@ -0,0 +1,330 @@
+"use client"
+import { Button } from "@/components/Button"
+import { countries } from "@/constants/countries"
+import { UploadOutlined } from "@ant-design/icons"
+import {
+  DatePicker,
+  Form,
+  Input,
+  Radio,
+  Select,
+  Space,
+  Upload,
+  Button as AntButton,
+} from "antd"
+import React, { FC } from "react"
+
+export const InternationalStep1: FC<{ next: () => void }> = ({ next }) => {
+  const [hasOtherName, setHasOtherName] = React.useState<"yes" | "no" | "">("")
+
+  return (
+    <div className="py-10 px-6 bg-white transition-all">
+      <Form layout="vertical" onFinish={next}>
+        <Form.Item
+          label="If you obtained this position, would you continue in any other employment OR take on any other employment? "
+          required
+          className="font-semibold"
+        >
+          <Radio.Group>
+            <Space direction="vertical">
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+            </Space>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item
+          label="Do we need to make any disability-related adjustments to allow you to take part in the recruitment process? "
+          required
+          className="font-semibold"
+        >
+          <Radio.Group>
+            <Space direction="vertical">
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+            </Space>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item
+          label="Do you have any mental health or physical health support requirements to be considered if offered a job?"
+          required
+          className="font-semibold"
+        >
+          <Radio.Group>
+            <Space direction="vertical">
+              <Radio value={"yes"}>Yes</Radio>
+              <Radio value={"no"}>No</Radio>
+            </Space>
+          </Radio.Group>
+        </Form.Item>
+        <div>
+          <h3 className="text-2xl font-semibold my-4">
+            Section 1:- Personal Details
+          </h3>
+          <div>
+            <Form.Item
+              label="Post Applied For:"
+              required
+              className="font-semibold"
+            >
+              <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+            </Form.Item>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item label="Title" required className="font-semibold">
+                <Select
+                  className="border-[#00000060] h-[2rem] w-full focus:border-success hover:border-success"
+                  style={{ height: "3.1rem" }}
+                  size="large"
+                  placeholder="Select"
+                  options={[
+                    { value: "Mr", label: "Mr" },
+                    { value: "Mrs", label: "Mrs" },
+                    { value: "Miss", label: "Miss" },
+                    { value: "Ms", label: "Ms" },
+                  ]}
+                />
+              </Form.Item>
+              <Form.Item label="First Name" required className="font-semibold">
+                <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+              <Form.Item label="Last Name" required className="font-semibold">
+                <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Have you ever been known by any other names?"
+                className="font-semibold"
+              >
+                <Radio.Group
+                  onChange={(e) => setHasOtherName(e.target.value)}
+                  value={hasOtherName}
+                >
+                  <Radio value={"yes"}>Yes</Radio>
+                  <Radio value={"no"}>No</Radio>
+                </Radio.Group>
+              </Form.Item>
+              {hasOtherName === "yes" && (
+                <Form.Item
+                  label="Enter Other Names"
+                  className="font-semibold md:col-span-2"
+                >
+                  <Input
+                    className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success"
+                    type="number"
+                  />
+                </Form.Item>
+              )}
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Home Telephone Number"
+                required
+                className="font-semibold"
+              >
+                <Input
+                  className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success"
+                  type="number"
+                />
+              </Form.Item>
+              <Form.Item
+                label="Mobile Telephone Number"
+                required
+                className="font-semibold"
+              >
+                <Input
+                  className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success"
+                  type="number"
+                />
+              </Form.Item>
+              <Form.Item label="Email" required className="font-semibold">
+                <Input
+                  className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success"
+                  type="email"
+                />
+              </Form.Item>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item label="Sex" required className="font-semibold">
+                <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+              <Form.Item
+                label="Date of Birth"
+                required
+                className="font-semibold "
+              >
+                <DatePicker className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+              <Form.Item
+                label="Are you aged 18 years or over?"
+                required
+                className="font-semibold"
+              >
+                <Radio.Group>
+                  <Radio value={"yes"}>Yes</Radio>
+                  <Radio value={"no"}>No</Radio>
+                </Radio.Group>
+              </Form.Item>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Nationality"
+                required
+                className="font-semibold "
+              >
+                <Select
+                  className="border-[#00000060] h-[2rem] w-full focus:border-success hover:border-success"
+                  style={{ height: "3.1rem" }}
+                  size="large"
+                  placeholder="Select"
+                  options={countries}
+                  showSearch
+                />
+              </Form.Item>
+              <Form.Item
+                label="Do you have a current full driving license:?"
+                required
+                className="font-semibold"
+              >
+                <Radio.Group>
+                  <Radio value={"yes"}>Yes</Radio>
+                  <Radio value={"no"}>No</Radio>
+                </Radio.Group>
+              </Form.Item>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Street Address"
+                required
+                className="font-semibold"
+              >
+                <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+              <Form.Item label="City" required className="font-semibold ">
+                <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+              <Form.Item label="Country" required className="font-semibold ">
+                <Select
+                  className="border-[#00000060] h-[2rem] w-full focus:border-success hover:border-success"
+                  style={{ height: "3.1rem" }}
+                  size="large"
+                  placeholder="Select"
+                  options={countries}
+                  showSearch
+                />
+              </Form.Item>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Zip/Postal Code"
+                required
+                className="font-semibold"
+              >
+                <Input className="border-[#00000060] p-[0.8rem] w-full focus:border-success hover:border-success" />
+              </Form.Item>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Copy of a valid passport"
+                required
+                className="font-semibold w-full"
+              >
+                <Upload accept=".pdf, .docx, .doc" className="w-full">
+                  <AntButton
+                    icon={<UploadOutlined />}
+                    size="large"
+                    className="border-[#00000060] w-full focus:border-success hover:border-success"
+                  >
+                    Click to Upload
+                  </AntButton>
+                </Upload>
+              </Form.Item>
+
+              <Form.Item
+                label="Passport size picture of yourself for ID purpose"
+                required
+                className="font-semibold w-full"
+              >
+                <Upload accept=".pdf, .docx, .doc" className="w-full">
+                  <AntButton
+                    icon={<UploadOutlined />}
+                    size="large"
+                    className="border-[#00000060] w-full focus:border-success hover:border-success"
+                  >
+                    Click to Upload
+                  </AntButton>
+                </Upload>
+              </Form.Item>
+
+              <Form.Item
+                label="Evidence of passed IELTS/ English Test"
+                required
+                className="font-semibold w-full"
+              >
+                <Upload accept=".pdf, .docx, .doc" className="w-full">
+                  <AntButton
+                    icon={<UploadOutlined />}
+                    size="large"
+                    className="border-[#00000060] w-full focus:border-success hover:border-success"
+                  >
+                    Click to Upload
+                  </AntButton>
+                </Upload>
+              </Form.Item>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <Form.Item
+                label="Proof of COVID vaccinations"
+                required
+                className="font-semibold w-full"
+              >
+                <Upload accept=".pdf, .docx, .doc" className="w-full">
+                  <AntButton
+                    icon={<UploadOutlined />}
+                    size="large"
+                    className="border-[#00000060] w-full focus:border-success hover:border-success"
+                  >
+                    Click to Upload
+                  </AntButton>
+                </Upload>
+              </Form.Item>
+
+              <Form.Item
+                label="Evidence of TB test result"
+                required
+                className="font-semibold w-full"
+              >
+                <Upload accept=".pdf, .docx, .doc" className="w-full">
+                  <AntButton
+                    icon={<UploadOutlined />}
+                    size="large"
+                    className="border-[#00000060] w-full focus:border-success hover:border-success"
+                  >
+                    Click to Upload
+                  </AntButton>
+                </Upload>
+              </Form.Item>
+            </div>
+          </div>
+        </div>
+        <div className="md:w-[60%] md:mx-auto my-[2rem]">
+          <Button
+            type="submit"
+            className="w-full rounded-[100px] text-xl"
+            style={{ borderRadius: "100px" }}
+          >
+            Next
+          </Button>
+        </div>
+      </Form>
+    </div>
+  )
+}
