a6d7c16505cc9da1970fd5f7c6a2026126aee6ef
RealRichi3 <molunorichie@gmail.com>
2024-01-18 19:21:02 +0100

    update links to licenses

--- app/LicensedBy.tsx
+++ app/LicensedBy.tsx
@@ -1,17 +1,26 @@
+import Link from "next/link"
 import React from "react"
 
 export const LicensedBy = () => {
-  return (
-    <div className="py-[2rem] md:py-[5rem] bg-white">
-      <h3 className="text-[24px] font-[600] text-center text-[#ACACAC] mb-[2rem]">
-        Licensed By
-      </h3>
-      <div className="flex md:flex-row flex-col justify-center items-center gap-[50px] md:gap-[119px]">
-        <img src="/license1.png" alt="" />
-        <img src="/license2.svg" alt="" />
-        <img src="/license3.png" alt="" />
-        <img src="/license.png" alt="" />
-      </div>
-    </div>
-  )
+    return (
+        <div className="py-[2rem] md:py-[5rem] bg-white">
+            <h3 className="text-[24px] font-[600] text-center text-[#ACACAC] mb-[2rem]">
+                Licensed By
+            </h3>
+            <div className="flex md:flex-row flex-col justify-center items-center gap-[50px] md:gap-[119px]">
+                <Link href={"https://www.homecareassociation.org.uk/"} target='_blank'>
+                    <img src="/license1.png" alt="" />
+                </Link>
+                <Link href={'https://www.skillsforcare.org.uk/Home.aspx'} target='_blank'>
+                    <img src="/license2.svg" alt="" />
+                </Link>
+                <Link href={'https://www.cqc.org.uk/'} target='_blank'>
+                    <img src="/license3.png" alt="" />
+                </Link>
+                <Link href={'https://www.hcpa.info/'} target='_blank'>
+                    <img src="/license.png" alt="" />
+                </Link>
+            </div>
+        </div>
+    )
 }
