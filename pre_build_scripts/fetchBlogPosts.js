e557f08be2cc7e41fe296125fa8f696eaeeeab82
RealRichi3 <molunorichie@gmail.com>
2024-05-21 18:06:41 +0100

    set default tag to hospitality

--- pre_build_scripts/fetchBlogPosts.js
+++ pre_build_scripts/fetchBlogPosts.js
@@ -1,15 +1,24 @@
-const { API_URL } = require('../constants/config.js')
-const axios = require('axios')
-const fs = require('fs')
+const { API_URL } = require("../constants/config.js");
+const axios = require("axios");
+const fs = require("fs");
 
 module.exports = class SeedBlogPostScript {
-    static start = async () => {
-        const response = await axios.get(API_URL + '/blog')
+  static start = async () => {
+    const response = await axios.get(API_URL + "/blog");
 
-        const sortedBlogPostsById = response.data.data.blogPosts.sort((a, b) => a.id - b.id)
+    const sortedBlogPostsById = response.data.data.blogPosts.sort(
+      (a, b) => a.id - b.id,
+    );
 
-        const jsonPath = __dirname + '/../db/data/posts.json'
-        fs.writeFileSync(jsonPath, JSON.stringify({ blogPosts: sortedBlogPostsById }))
-    }
-}
-
+    const jsonPath = __dirname + "/../db/data/posts.json";
+    fs.writeFileSync(
+      jsonPath,
+      JSON.stringify({
+        blogPosts: sortedBlogPostsById.map((post) => ({
+          ...post,
+          tags: ["hospitality"],
+        })),
+      }),
+    );
+  };
+};
