ab690f5ca3a57b562b95e67aff7f84c4c2368507
Osaretin Frank <osaretin.frank10@gmail.com>
2024-07-18 23:45:06 +0100

    update base api url

--- constants/config.ts
+++ constants/config.ts
@@ -2,5 +2,5 @@ export const MAILCHIMP_FORM_URL =
   "https://gmail.us21.list-manage.com/subscribe/post-json?u=5f10c149c3cf08fb69cf12a7b&amp;id=27df81fde2&amp;f_id=00e62ae7f0"
 export const CALENDLY_SCHEDULE_URL = "https://calendly.com/molunorichie/30min"
 // export const API_URL = "https://flourishcare-api.onrender.com/api/v1"
-export const API_URL = "http://194.164.93.163:5000/"
+export const API_URL = "http://194.164.93.164:5000/api/v1"
 export const API_URL_V1 = "https://bluemarvel-mail-server.onrender.com"
