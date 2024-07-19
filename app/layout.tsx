cadf2e6e4571cbeb6672af1b54b1ff3cff721872
Osaretin Frank <osaretin.frank10@gmail.com>
2024-07-19 11:42:28 +0100

    update opengraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,27 @@ import { AntProvider } from "@/components/AntProvider"
 import ReduxProvider from "@/components/ReduxProvider"
 import { ToastContainer } from "react-toastify"
 import "animate.css/animate.min.css"
+import { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Flourish Advanced Care",
+  description:
+    "Flourish Advanced Care is rooted in the mission to deliver care with integrity and compassion. Our team is carefully selected and trained to match the needs of our clients with the utmost respect and professionalism.",
+  openGraph: {
+    title: "Flourish Advanced Care",
+    siteName: "Flourish Advanced Care",
+    description:
+      "Flourish Advanced Care is rooted in the mission to deliver care with integrity and compassion. Our team is carefully selected and trained to match the needs of our clients with the utmost respect and professionalism.",
+
+    images: [
+      {
+        url: "/opengraph-image.png",
+      },
+    ],
+  },
+}
 
 export default function RootLayout({
   children,
